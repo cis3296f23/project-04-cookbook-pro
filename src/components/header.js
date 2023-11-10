efc2c03969a50c1cb423135f4ec65daea31cd887
JayLiuTU <tuj76761@temple.edu>
2023-11-10 17:13:33 -0500

    sticky navbar

--- src/components/header.js
+++ src/components/header.js
@@ -5,14 +5,14 @@ import { Navbar } from 'reactstrap';
 export default () => {
 
     return (
-        <nav class="navbar navbar-expand-lg bg-light">
+        <nav class="navbar fixed-top navbar-expand-lg bg-light">
             <div class="container-fluid justify-content-start">
                 <a class="navbar-brand" href="/">CookBook Pro</a>
                 <div class="navbar-nav">
-                    <a class="nav-link" href="\meal-plans">Meal Plans</a>
-                    <a class="nav-link" href="\search">Search</a>
-                    <a class="nav-link" href="\recipes">Recipes</a>
-                    <a class="nav-link" href="\shopping-list">Shopping List</a>
+                    <a class="nav-link" href="/meal-plans">Meal Plans</a>
+                    <a class="nav-link" href="/search">Search</a>
+                    <a class="nav-link" href="/recipes">Recipes</a>
+                    <a class="nav-link" href="/shopping-list">Shopping List</a>
                 </div>
             </div>
         </nav>
