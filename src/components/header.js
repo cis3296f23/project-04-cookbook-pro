55a203cab68cd6a81b3cb9bf17c411edc3abfb81
JayLiuTU <tuj76761@temple.edu>
2023-11-09 23:06:15 -0500

    removed unused elements

--- src/components/header.js
+++ src/components/header.js
@@ -5,25 +5,17 @@ import { Navbar } from 'reactstrap';
 export default () => {
 
     return (
-
-        <nav class="navbar navbar-expand-lg navbar-light bg-light ">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="/">CookBook Pro</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-                <a class="nav-link" href="\meal-plans">Meal Plans</a>
-                <a class="nav-link" href="\search">Search</a>
-                <a class="nav-link" href="\recipes">Recipes</a>
-                <a class="nav-link" href="\shopping-list">Shopping List</a>
+        <nav class="navbar navbar-expand-lg bg-light">
+            <div class="container-fluid justify-content-start">
+                <a class="navbar-brand" href="/">CookBook Pro</a>
+                <div class="navbar-nav">
+                    <a class="nav-link" href="\meal-plans">Meal Plans</a>
+                    <a class="nav-link" href="\search">Search</a>
+                    <a class="nav-link" href="\recipes">Recipes</a>
+                    <a class="nav-link" href="\shopping-list">Shopping List</a>
+                </div>
             </div>
-            </div>
-        </div>
         </nav>
-
     )
-
 
 };
