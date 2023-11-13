423bf567449a1015d3e5b0110999a9bbd7c319e0
zyij99 <tum96927@temple.edu>
2023-11-13 16:23:40 -0500

    moved and renamed firebase config file

--- src/components/signInButton.js
+++ src/components/signInButton.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
-import {auth} from '../firebaseConfig.js';
+import {auth} from '../firebase/firebase.js';
 
 
 const SignInButton = () => {
 
   const handleGoogle = async (e) => {
-    const provider = await new GoogleAuthProvider();
+    const provider = await new GoogleAuthProvider(); //Wait for popup Auth
     return signInWithPopup(auth, provider)
     .then((result) => {
       const user = result.user;
