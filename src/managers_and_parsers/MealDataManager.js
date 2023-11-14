2a190c4b6b1337d309ba73095d3f4b7d8f498e5f
JayLiuTU <tuj76761@temple.edu>
2023-11-14 10:15:06 -0500

    apped params to get more info and 100 recipes

--- src/managers_and_parsers/MealDataManager.js
+++ src/managers_and_parsers/MealDataManager.js
@@ -2,43 +2,69 @@
 // 1. The Spoonacular API
 // 2. The FirebaseDB
 // Ingredients as well as Meals will be handled by this manager (for now)
-import { PrefabMeal } from '../objects_JSON/objects.js';
+import { PrefabMeal } from "../objects_JSON/objects.js";
 class MealDataManager {
-    // spoonacularParser = new SpoonacularParser();
-    constructor() {
-        // https://spoonacular.com/food-api/console#Dashboard
-        this.spoonacularURL = new URL("https://api.spoonacular.com/recipes");
-        this.spoonacularApi = process.env.REACT_APP_SPOONACULAR_API_KEY;
+  // spoonacularParser = new SpoonacularParser();
+  constructor() {
+    // https://spoonacular.com/food-api/console#Dashboard
+    this.spoonacularURL = new URL("https://api.spoonacular.com/recipes");
+    this.spoonacularApi = process.env.REACT_APP_SPOONACULAR_API_KEY;
+  }
+
+  async queryRecipeFromSpoonacular(query) {
+    //add in these parameters
+    /*
+        addRecipeInformation	boolean	false	If set to true, you get more information about the recipes returned.
+        offset                  number	0	    The number of results to skip (between 0 and 900).
+        number  	            number	10	    The number of expected results (between 1 and 100).
+        fillIngredients	        boolean	false	Add information about the ingredients and whether they are used or missing in relation to the query.
+        */
+    const searchQuery = new URLSearchParams();
+    searchQuery.append("apiKey", this.spoonacularApi);
+    searchQuery.append("query", query); // Assuming query is a string, adjust accordingly
+    searchQuery.append("addRecipeInformation", true);
+    searchQuery.append("offset", 0); //increment offset to get more results of current query
+    searchQuery.append("number", 100); //ask for 100 recipes instead of 10
+    searchQuery.append("fillIngredients", true); //get ingredient info
+
+    const fullUrl = `${
+      this.spoonacularURL
+    }/complexSearch?${searchQuery.toString()}`;
+
+    console.log("api query = " + fullUrl);
+
+    /*
+        using the search params from above we get these properities of a recipe
+        recipe keys=["vegetarian","vegan","glutenFree","dairyFree","veryHealthy","cheap","veryPopular","sustainable","lowFodmap","weightWatcherSmartPoints","gaps","preparationMinutes","cookingMinutes","aggregateLikes","healthScore","creditsText","sourceName","pricePerServing","extendedIngredients","id","title","readyInMinutes","servings","sourceUrl","image","imageType","summary","cuisines","dishTypes","diets","occasions","analyzedInstructions","spoonacularScore","spoonacularSourceUrl","usedIngredientCount","missedIngredientCount","missedIngredients","likes","usedIngredients","unusedIngredients"]
+        */
+
+    try {
+      const response = await fetch(fullUrl);
+      const data = await response.json();
+
+      const searchResultsList = data.results.map((recipe) => {
+        // Instantiate PrefabMeal for each result
+
+        const keys = JSON.stringify(Object.keys(recipe));
+        console.log("recipe keys=" + keys);
+
+        const mappedResult = new PrefabMeal(
+          recipe.title,
+          recipe.extendedIngredients,
+          recipe.analyzedInstructions,
+          recipe.image,
+          recipe.id,
+          recipe.summary
+        );
+
+        return mappedResult;
+      });
+
+      return searchResultsList;
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      throw error;
     }
-
-    async queryRecipeFromSpoonacular(query) {
-        const searchQuery = new URLSearchParams();
-        searchQuery.append('apiKey', this.spoonacularApi);
-        searchQuery.append('query', query); // Assuming query is a string, adjust accordingly
-
-        const fullUrl = `${this.spoonacularURL}/complexSearch?${searchQuery.toString()}`;
-
-        try {
-            const response = await fetch(fullUrl);
-            const data = await response.json();
-
-            const searchResultsList = data.results.map(recipe => {
-                // Instantiate PrefabMeal for each result
-                const mappedResult = new PrefabMeal(
-                    recipe.title,
-                    recipe.extendedIngredients,
-                    recipe.instructions,
-                    recipe.image
-                );
-
-                return mappedResult;
-            });
-
-            return searchResultsList;
-        } catch (error) {
-            console.error("Error fetching data:", error);
-            throw error;
-        }
-    }
+  }
 }
 export default MealDataManager;
