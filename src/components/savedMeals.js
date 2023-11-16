8c1bcdbf6b23b1472e65bddb4c0f1d39dba614eb
JayLiuTU <tuj76761@temple.edu>
2023-11-16 12:25:32 -0500

    clicking a saved meal now opens the details in recipeDetails.js it can unsave the recipe

--- src/components/savedMeals.js
+++ src/components/savedMeals.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect }  from "react";
+import React, { useState, useEffect } from "react";
 import getListener from "../firebase/setListener.js";
 import {
   Card,
@@ -9,26 +9,42 @@ import {
   ListInlineItem,
 } from "reactstrap";
 
-
+import RecipeDetails from "./recipeDetails.js";
 
 const savedMeals = () => {
-    const [savedRecipes, setSavedRecipes] = useState([""]);
+  const [savedRecipes, setSavedRecipes] = useState([""]);
+  const [showDetails, setShowDetails] = useState(false);
+  const [meal, setMeal] = useState();
 
+  const toggle = (recipe) => {
+    setMeal(recipe);
+    setShowDetails(!showDetails);
+  };
 
-    //important to only get 1 listener, so use this thingy
-    useEffect(()=>{
-        const unsubscibe = getListener("savedRecipes", setSavedRecipes);
-    }, [])
+  //important to only get 1 listener, so use this thingy
+  useEffect(() => {
+    const unsubscibe = getListener("savedRecipes", setSavedRecipes);
+  }, []);
 
+  let recipeDetails;
+
+  if (showDetails) {
+    recipeDetails = (
+      <RecipeDetails meal={meal} showDetails={showDetails} toggle={toggle} />
+    );
+  }
 
   return (
     <ListGroup>
-      <ListGroupItemHeading >My Recipes</ListGroupItemHeading>
-      
-      { Object.entries(savedRecipes).map(([key, value]) => {
-        return <ListGroupItem key={key}>{value}</ListGroupItem>
-    })}
-      
+      <ListGroupItemHeading>My Recipes</ListGroupItemHeading>
+      {recipeDetails}
+      {savedRecipes.map((recipe, key) => {
+        return (
+          <ListGroupItem action onClick={() => toggle(recipe)} key={key}>
+            {recipe.name}
+          </ListGroupItem>
+        );
+      })}
     </ListGroup>
   );
 };
