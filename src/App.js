cef74914e33f5c62ccddc276c77efa67fa751d4e
defining-art <118932320+defining-art@users.noreply.github.com>
2023-11-20 01:04:58 -0500

    isNavBar?

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { StrictMode } from "react";
 import "./styles.css";
 
 //single page application stuff
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 //import my header navbar
 import Navbar from "./components/header.js";
@@ -14,22 +14,27 @@ import Search from "./pages/search.js";
 import MealPlans from "./pages/mealPlans.js";
 import Recipes from "./pages/recipes.js";
 import ShoppingList from "./pages/shoppingList.js";
-import SignIn from "./pages/signin.js";
+//import SignIn from "./pages/signin.js";
+import Login from "./pages/Login.js";
+import SignUp from "./pages/SignUp.js";
+import HasNav from "./components/hasNav.js";
 
-function App() {
-  return (
-    //a router watches the url for changes and will re-render the page
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" exact element={<Home />} />
-        <Route path="/search" exact element={<Search />} />
-        <Route path="/meal-plans" exact element={<MealPlans />} />
-        <Route path="/recipes" exact element={<Recipes />} />
-        <Route path="/shopping-list" exact element={<ShoppingList />} />
-        <Route path="/sign-in" exact element={<SignIn />} />
-      </Routes>
-    </Router>
+function App(){
+  return( 
+  <Router>
+    <HasNav>
+      <Navbar/>
+    </HasNav>
+    <Routes>
+      <Route path="/" exact element={<Home />} />
+      <Route path="/search" exact element={<Search />} />
+      <Route path="/meal-plans" exact element={<MealPlans />} />
+      <Route path="/recipes" exact element={<Recipes />} />
+      <Route path="/shopping-list" exact element={<ShoppingList />} />
+      <Route path="/Login" exact element={<Login />} />
+      <Route path="/SignUp" exact element={<SignUp />} />
+    </Routes>
+  </Router>
   );
 }
 
