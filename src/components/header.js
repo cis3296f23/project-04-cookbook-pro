2c667bf96675043230caeaec8567778a00577a7f
JayLiuTU <tuj76761@temple.edu>
2023-11-20 20:54:55 -0500

    refactored navbar to use reactstrap

--- src/components/header.js
+++ src/components/header.js
@@ -1,37 +1,45 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  Nav,
+  NavLink,
+  NavItem,
+  Container,
+  Button,
+} from "reactstrap";
 
 export default () => {
   return (
-    <nav className="navbar navbar-expand-lg bg-light" style={{ height:"7vh", width: "100%", overflowY: "auto" }}>
-      <div className="container-fluid justify-content-start">
-        <a className="navbar-brand" href="/">
-          CookBook Pro
-        </a>
-        <div className="navbar-nav mr-auto">
-          <a className="nav-link" href="/meal-plans">
-            Meal Plans
-          </a>
-          <a className="nav-link" href="/search">
-            Search
-          </a>
-          <a className="nav-link" href="/recipes">
-            Recipes
-          </a>
-          <a className="nav-link" href="/shopping-list">
-            Shopping List
-          </a>
-          <a id={"buffer"} style={{width:"950px"}}></a>
-          <a className="nav-link" href="/Login">
-            <input
-              className="getLogin"
-              type="button"
-              id="isValid"
-              value={"Log In"}
-            />
-          </a>
-        </div>
-      </div>
-    </nav>
+    <Navbar className="navbar-expand bg-light">
+      <NavbarBrand className="" href="/">
+        CookBook Pro
+      </NavbarBrand>
+      <Nav className="navbar-nav me-auto">
+        <NavItem>
+          <NavLink href="/meal-plans">Meal Plans</NavLink>
+        </NavItem>
+
+        <NavItem>
+          <NavLink href="/search">Search</NavLink>
+        </NavItem>
+
+        <NavItem>
+          <NavLink href="/recipes">Recipes</NavLink>
+        </NavItem>
+
+        <NavItem>
+          <NavLink href="/shopping-list">Shopping List</NavLink>
+        </NavItem>
+
+        <NavItem>
+          <Container></Container>
+        </NavItem>
+      </Nav>
+      <Button className="" href="/Login">
+        Login
+      </Button>
+    </Navbar>
   );
 };
