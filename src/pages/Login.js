0f1b4d9c513fca9492d2c742cbc748d2685fb514
zyij99 <tum96927@temple.edu>
2023-11-28 17:20:21 -0500

    added background image to the login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,46 +1,93 @@
-import React, {useState, useEffect} from 'react'
-import {getAuth, signInWithEmailAndPassword} from 'firebase/auth';
-import '../index.js'
+import React, { useState, useEffect } from 'react';
+import { getAuth, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import '../index.js';
 import './LoginSignUp.css';
 import '../firebase/firebaseConfig.js';
-import './home.js'
-import './SignUp.js'
+import './home.js';
+import './SignUp.js';
 
-var Login=()=>{
-  const[userEmail, isUserEmail] = useState("");
-  const[userPassword, isUserPassword] = useState("");
+const Login = () => {
+  const [userEmail, isUserEmail] = useState('');
+  const [userPassword, isUserPassword] = useState('');
   const auth = getAuth();
-  useEffect(()=>{
+
+  useEffect(() => {
     document.title = 'CookBook-Pro: Login';
-    document.body.style.backgroundColor="#E0EAFC";
+    document.body.classList.add('loginPage');
+    document.body.style.backgroundColor = '#E0EAFC';
   }, []);
-  const checkInput=async(e)=>{
+
+  const checkInput = async (e) => {
     e.preventDefault();
-    await signInWithEmailAndPassword(auth, userEmail, userPassword)
-    .then((userCredential)=>{
+    try {
+      const userCredential = await signInWithEmailAndPassword(auth, userEmail, userPassword);
       const user = userCredential.user;
-      document.location.href = "/";
-    }) 
-    .catch((error)=>{
+      document.location.href = '/';
+    } catch (error) {
       const errorCode = error.code;
       const errorMessage = error.message;
-      console.log(errorCode,errorMessage);
-    });
-  }
+      console.log(errorCode, errorMessage);
+    }
+  };
+
+  const signInWithGoogle = async () => {
+    const provider = new GoogleAuthProvider();
+    try {
+      const userCredential = await signInWithPopup(auth, provider);
+      const user = userCredential.user;
+      isUserEmail(user.email);
   
-  return(
-    <div className = "LogIn">
-      <h1 className="Title">Log In</h1><br />
+      document.location.href = '/';
+    } catch (error) {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode, errorMessage);
+    }
+  };
+
+  return (
+    <div className="LogIn">
+      <h1 className="Title">Log In</h1>
+      <br />
+        <center>
+          <button type="button" className="googleSignInButton" onClick={signInWithGoogle}>
+            Log In with Google
+          </button>
+        </center>
       <form>
         <label className="Email">Email<br></br></label>
-        <input value={userEmail} onChange={e => isUserEmail(e.target.value)} className = "getEmail" type="text" id="isEmail" name="isEmail"/><br/>
+        <input
+          value={userEmail}
+          onChange={(e) => isUserEmail(e.target.value)}
+          className="getEmail"
+          type="text"
+          id="isEmail"
+          name="isEmail"
+        />
+        <br />
         <label className="Password">Password<br></br></label>
-        <input value={userPassword} onChange={e => isUserPassword(e.target.value)} className="getPassword" type="text" id="isPassword" name="isPassword"/>
-        <div className="InvalidInput"><span style={{color:"blue"}}>Forgot Password?</span></div><br/>
-        <input className= "isValidate" type="button" id="isValid" onClick={e=>checkInput(e)} value={"Log In"} />
-      </form><br></br>
-      <p style={{textAlign:'center'}}><a href='/SignUp'>Dont Have An Account? Create Account</a></p>
+        <input
+          value={userPassword}
+          onChange={(e) => isUserPassword(e.target.value)}
+          className="getPassword"
+          type="text"
+          id="isPassword"
+          name="isPassword"
+        />
+        <div className="InvalidInput">
+          <span style={{ color: 'blue' }}>Forgot Password?</span>
+        </div>
+        <br />
+        <input className="isValidate" type="button" id="isValid" onClick={(e) => checkInput(e)} value={'Log In'} />
+        <br />
+        
+      </form>
+      <br></br>
+      <p style={{ textAlign: 'center' }}>
+        <a href="/SignUp">Don't Have An Account? Create Account</a>
+      </p>
     </div>
-  )
-}
+  );
+};
+
 export default Login;
